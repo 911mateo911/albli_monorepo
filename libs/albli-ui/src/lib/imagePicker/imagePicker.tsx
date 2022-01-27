83283e8eb2ce8b0a5391ec8a71d7b689a3c0e7a6
911mateo911 <malajmateo@gmail.com>
2022-01-28 00:18:51 +0100

    create imagePicker component, refactor dropdown

--- libs/albli-ui/src/lib/imagePicker/imagePicker.tsx
+++ libs/albli-ui/src/lib/imagePicker/imagePicker.tsx
@@ -1,8 +1,40 @@
-import { FC } from 'react';
+import { FC, useState, useEffect, useRef, ChangeEvent } from 'react';
 import styles from './imagePicker.module.scss';
 import { ReactComponent as CameraIcon } from '../icons/camera.svg';
+import { ImageType } from './imagePicker.interface';
 
 export const ImagePicker: FC = () => {
+    const [images, setImages] = useState<ImageType[]>([]);
+    const prevImages = useRef<ImageType[]>([]);
+
+    const handleChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+        const files = target.files;
+
+        if (!files) return;
+
+        const withPreviewFiles: ImageType[] = [];
+
+        // for loop as we cant iterate through a FileList
+
+        for (let index = 0; index < Number(files?.length); index++) {
+            withPreviewFiles.push(Object.assign(
+                files[index],
+                {
+                    preview: URL.createObjectURL(files[index])
+                }
+            ));
+        }
+
+        setImages(withPreviewFiles);
+    }
+
+    useEffect(() => {
+        // prevent memory leaks
+        prevImages.current.map(({ preview }) => URL.revokeObjectURL(preview));
+
+        prevImages.current = images;
+    }, [images]);
+
     return (
         <div className={styles['picker-container']}>
             <label className={styles['picker-label']} htmlFor='image-picker' ></label>
@@ -13,6 +45,7 @@ export const ImagePicker: FC = () => {
                 type='file'
                 aria-label='image-picker'
                 id='image-picker'
+                onChange={handleChange}
                 name='image-picker'
             />
             <p className={styles['picker-text']} >Ngarko fotografi</p>
